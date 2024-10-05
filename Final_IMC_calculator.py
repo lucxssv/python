b978f0b6c2d7e81cc641cb75c1045f9536cdd839
Luc <57410577+lucxssv@users.noreply.github.com>
2024-10-05 15:38:50 -0300

    Update Final_IMC_calculator.py

--- Final_IMC_calculator.py
+++ Final_IMC_calculator.py
@@ -69,6 +69,8 @@ while continuar == 1:
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#FINAL VERSION!
+
 def imc (peso, altura):
   altura_quadrada = altura ** 2
   meu_imc = peso / altura_quadrada
@@ -96,54 +98,62 @@ def imc (peso, altura):
       print ('grau_obesidade = "III"')
       print('---------------------------------------------------------------')
   
+comecar = 1
 continuar = 1
 
-while continuar == 1:
+cont = 1
+cont2 = 1
 
+while True:
     print("\n")
     print('Bem vindo a calculador de IMC (Índice de massa corporal), deseja calcular?')
-    continuar = int(input('Digite 1 para SIM e  0 para NÃO: '))
+    comecar = int(input('Digite 1 para SIM e  0 para NÃO: '))
 
-    if continuar == 0:
+    if comecar == 0:
         print("\n")
         print('Fim da execução')
-        print("\n")
         break
     else:
-        continuar = 1
+        comecar = 1
+
+    ps = 0  # redefine ps
+    alt = 0  # redefine alt
 
     print("\n")
 
-    while 1:
+    while cont == 1:
+        print("\n")
         ps = float(input('Digite seu peso em kilos : '))
 
-        if 10 > ps or ps > 500: 
+        if 10 > ps or ps >= 500: 
             print('Fora dos limites, use numeros válidos')
-            break
+            cont = 1
         else:
-        
-            print("\n")
-            alt = float(input('Digite sua altura em metros (separanndo as casas por "."): '))
+            break
 
-            if 0.10 > alt or alt > 2.50:
-                print('Fora dos limites, use numeros válidos')
-                break
-            else:
-                print("\n")
-                meu_imc = imc(ps, alt)
-                break
+    while cont2 == 1:
 
-    print("\n")
+        print("\n")
+        alt = float(input('Digite sua altura em metros (separanndo as casas por "."): '))
+
+        if 0.10 >= alt or alt >= 2.52:
+            print('Fora dos limites, use numeros válidos')
+            cont2 = 1
+        else:
+            break
+
+    print ("\n")
+    meu_imc = imc(ps, alt)
+    print ("\n")
 
     print('Deseja calcular mais uma vez?')
     continuar = int(input('Digite 1 para SIM e  0 para NÃO: '))
+
     if continuar == 0:
         print("\n")
         print('Fim da execução')
-        print("\n")
         break
     else:
         continuar = 1
-
 
 
